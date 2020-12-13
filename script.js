4dffe999d360584fde6ca8a5986cc3420e6849f7
kritika <kritikasri27@gmail.com>
2020-12-13 21:50:45 +0530

    Added logic of creating Joke teller App through Joke API

---
+++ script.js
@@ -0,0 +1,50 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// VoiceRSS Speech Function
+function tellMe(joke) {
+  const jokeString = joke.trim().replace(/ /g, "%20");
+  // VoiceRSS Speech Parameters
+  VoiceRSS.speech({
+    key: "7d2f15211562445bb5a6c0e28e0cd027",
+    src: jokeString,
+    hl: "en-us",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
+
+// Get jokes from Joke API
+async function getJokes() {
+  let joke = "";
+  const apiUrl =
+    "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist&type=single";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    // Assign One or Two Part Joke
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    // Passing Joke to VoiceRSS API
+    tellMe(joke);
+    // Disable Button
+    toggleButton();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
